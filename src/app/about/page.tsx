3afc5f7082d1897ea5602c3ac655e3f8e127ae9b
Noor Fatima <160162835+NoorFatima7654@users.noreply.github.com>
2024-12-02 18:09:11 +0500

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,37 +1,49 @@
-import React from 'react'
-import Image from 'next/image'
+
+      
+      
+
+
+
+
+      
+import React from 'react';
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className=' h-screen  bg-gray-400 flex justify-center items-center'>
-      <div className='flex h-[465] w-11/12'>
-      <div className=' flex flex-col flex-1 gap-4 justify-center px-4'>
-      <h1 className='font-bold  text-6xl'> Noor Fatima</h1>
-      <p className='font-normal text-3xl'>Hi, I`m Noor Fatima, a  highly skilled web developer with extensive 
-        expertise in creating dynamic
-      
-      and responsive web applications.  
+    <div className="h-screen bg-gray-400 flex justify-center items-center px-4">
+      <div className="flex flex-col md:flex-row md:h-auto w-full md:w-10/12 bg-white rounded-lg shadow-lg">
+        {/* Text Section */}
+        <div className="flex flex-col flex-1 gap-4 justify-center p-6 text-center md:text-left">
+          <h1 className="font-bold text-3xl mt-4 sm:text-4xl md:text-5xl">Noor Fatima</h1>
+          <p className="font-normal text-sm sm:text-base md:text-lg">
+            Hi, I`m Noor Fatima, a highly skilled web developer with extensive
+            expertise in creating dynamic and responsive web applications.
+            <br />
+            <br />
+            Currently, I`m part of an amazing training program at the Governor
+            House, which has helped me grow tremendously. This program is guided
+            by some of the most incredible mentors.
+            <br />
+            <br />
+            In addition to my web development skills, I have strong command over
+            programming languages such as HTML, CSS, JavaScript, TypeScript, and
+            Next.js.
+          </p>
+        </div>
 
-   
-       Currently, I`m part of an amazing training program at the Governor House, which has helped me grow tremendously.
-        This program is guided by some of the most incredible mentors.
-
-In addition to my web development skills, I have strong command over programming languages such as HTML, CSS, JavaScript, TypeScript, and Next.js..
-
-</p>
-</div>
-      <div className=' flex-1'> 
-        <div>
-      <Image src='/images/noor.jpeg'
-      alt="picture"
-      width={400}
-      height={400}
-      className='rounded-full mr-6'/>
+        {/* Image Section */}
+        <div className="flex flex-1 justify-center items-center p-6">
+          <Image
+            src="/images/noor.jpeg"
+            alt="Noor Fatima"
+            width={150} // Default for small screens
+            height={150}
+            className="rounded-full sm:w-40 sm:h-40 md:w-56 md:h-56 lg:w-64 lg:h-64"
+          />
+        </div>
       </div>
-    </div>
-    </div>
-    </div>
-  )
+    </div>
+  );
 }
-      
-      
+  
